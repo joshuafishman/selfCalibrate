97e56dd6cdcbe3f68599f14ca574f17bd895181f
joshua fishman <joshuafishman1@gmail.com>
2016-08-30 14:55:21 -0400

    2nd try for path input

--- RUNcalibration.py
+++ RUNcalibration.py
@@ -8,13 +8,20 @@ Created on Wed Aug 03 13:19:36 2016
 import refractiveSelfCalibration #autocalibration library
 import yaml
 import sys
+import os.path 
 
 if len(sys.argv) > 1:
     configPath = sys.argv[1]
 else:    
     configPath = raw_input('Input the config file please: ')
-    configPath = 'config.txt' if configPath == '' else configPath #hit enter to just use local 'config.txt' as the config file
+    configPath = 'config.txt' if configPath == '' else configPath 
 
+while not os.path.isfile(configPath):
+    configPath = raw_input('Incorrect path. Please input the config file again, or type exit to quit: ')  
+    if configPath == 'exit': #hopefully this lets you quit the raw_input
+        sys.exit()
+
+    
 #config file must have exactly correct variable names
 config = yaml.load(open(configPath , 'r'))
 
@@ -23,7 +30,7 @@ for name,var in config.iteritems():
         try:
             config[name] = float(var) #make sure everything that's supposed to be a number is
         except:
-            raise Exception (name + ' has an invalid value.')
+            raise ValueError (name + ' has an invalid value.')
               
 globals().update(config) #add all the variables in the config file to the global namespace
 
