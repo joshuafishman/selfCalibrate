4fe52f47f698ac3738ca425582a3d2b29eadcfe9
joshua fishman <joshuafishman1@gmail.com>
2016-09-01 15:52:44 -0400

    generalized argument names

--- RUNcalibration.py
+++ RUNcalibration.py
@@ -8,34 +8,41 @@ Created on Wed Aug 03 13:19:36 2016
 import refractiveSelfCalibration #autocalibration library
 import yaml 
 import argparse
-
+import inspect
 
 parser = argparse.ArgumentParser()
 parser.add_argument("configPath", help = "Config file path")
 
 configPath = parser.parse_args().configPath   
 
-#config file must have exactly correct variable names
+#config file must have exactly correct variable names, ignoring case
 with open(configPath , 'r') as f: 
-    config = yaml.load(f)
+    config = {name.lower(): value for name,value in yaml.load(f).iteritems()}
     
-#these are the expected variables in config. Order matters.
-if config['image_type'] == 'multi':            
-    argnames = ["dataPath","exptPath","camIDs","image_type","dX","dY","nX","nY","nCalPlanes","znet","n1","n2","n3","tW","zW","tol","fg_tol","maxiter","bi_tol","bi_maxiter","z3_tol","rep_err_tol","sX","sY","pix_Pitch","so","f","nFrames"]
-else:
-    argnames = ["dataPath","exptPath","camIDs","image_type","dX","dY","nX","nY","nCalPlanes","znet","n1","n2","n3","tW","zW","tol","fg_tol","maxiter","bi_tol","bi_maxiter","z3_tol","rep_err_tol","sX","sY","pix_Pitch","so","f"]
-
+args = inspect.getargspec(refractiveSelfCalibration.Calibration) #arguments for calibration
 values = []
-for name in argnames:   
-    try:
-        if name in ['dataPath','exptPath','camIDs', 'image_type']: #these values should be strings
+
+try:
+    
+    for name in args[0][:-len(args[3])]:   #non-optional values
+        if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
             values.append(config[name])
         else:
             values.append(float(config[name]))  #make sure everything that's supposed to be a number is
-    except KeyError:
-        raise KeyError ("Missing " + name + " value in config file.")
-    except ValueError:             
-        raise ValueError (name + ' has an invalid value.')
+    
+    for name in args[0][-len(args[3]):]:   #optional values
+        try:
+            if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
+                values.append(config[name.lower()])
+            else:
+                values.append(float(config[name.lower()]))  #make sure everything that's supposed to be a number is
+        except KeyError:
+            continue
+    
+except KeyError:
+    raise KeyError ("Missing " + name + " value in config file.")
+except ValueError:             
+    raise ValueError (name + ' has an invalid value.')
 
 refractiveSelfCalibration.Calibration(*values)
 
