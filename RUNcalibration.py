e579d35589b64e28e1649585f904fe8fbe2b2893
joshua fishman <joshuafishman1@gmail.com>
2016-09-01 16:27:46 -0400

    fixed #3

--- RUNcalibration.py
+++ RUNcalibration.py
@@ -10,39 +10,50 @@ import yaml
 import argparse
 import inspect
 
+
+def parseConfig (conf): 
+    """
+    parse the config parameters
+    :param conf: dictionary of config parameters
+    :returns:    list of correctly ordered arguments for Calibration
+    """
+    
+    conf_low = {name.lower(): value for name,value in conf.iteritems()} #config file must have exactly correct variable names, ignoring case
+    
+    args = inspect.getargspec(refractiveSelfCalibration.Calibration) #arguments for calibration
+    values = []
+    
+    try:
+        
+        for name in args[0][:-len(args[3])]:   #non-optional values
+            if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
+                values.append(conf_low[name])
+            else:
+                values.append(float(conf_low[name]))  #make sure everything that's supposed to be a number is
+        
+        for name in args[0][-len(args[3]):]:   #optional values
+            try:
+                if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
+                    values.append(conf_low[name.lower()])
+                else:
+                    values.append(float(conf_low[name.lower()]))  #make sure everything that's supposed to be a number is
+            except KeyError:
+                continue
+        
+    except KeyError:
+        raise KeyError ("Missing " + name + " value in config file.")
+    except ValueError:             
+        raise ValueError (name + ' has an invalid value.')
+    
+    return values
+    
 parser = argparse.ArgumentParser()
 parser.add_argument("configPath", help = "Config file path")
 
 configPath = parser.parse_args().configPath   
 
-#config file must have exactly correct variable names, ignoring case
 with open(configPath , 'r') as f: 
-    config = {name.lower(): value for name,value in yaml.load(f).iteritems()}
+    config = yaml.load(f)
     
-args = inspect.getargspec(refractiveSelfCalibration.Calibration) #arguments for calibration
-values = []
-
-try:
-    
-    for name in args[0][:-len(args[3])]:   #non-optional values
-        if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
-            values.append(config[name])
-        else:
-            values.append(float(config[name]))  #make sure everything that's supposed to be a number is
-    
-    for name in args[0][-len(args[3]):]:   #optional values
-        try:
-            if name.lower() in ['datapath','exptpath','camids', 'image_type']: #these values should be strings
-                values.append(config[name.lower()])
-            else:
-                values.append(float(config[name.lower()]))  #make sure everything that's supposed to be a number is
-        except KeyError:
-            continue
-    
-except KeyError:
-    raise KeyError ("Missing " + name + " value in config file.")
-except ValueError:             
-    raise ValueError (name + ' has an invalid value.')
-
-refractiveSelfCalibration.Calibration(*values)
+refractiveSelfCalibration.Calibration(*parseConfig(config))
 
